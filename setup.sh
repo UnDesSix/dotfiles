33a2a7d1ea5e350a5ce30db5704a1a74bf3d1b90
mlarboul <mlarboul@student.42.fr>
2021-05-18 07:22:07 +0200

    Setup file for ubuntu + i3wm config file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!bin/sh/
+
+sudo apt install vim make clang git nasm wget curl zsh i3 -y;
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+chsh -s $(which zsh);
+echo "Please log out and come back\n";
+
+
