09f2773b57cd08b09e552e9f4ef63090688e40c1
Matthieu L <matthieu.larboullet@gmail.com>
2021-07-03 19:09:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!bin/sh/
 
-sudo apt install vim make clang git nasm wget curl zsh i3 -y;
+sudo apt install vim make gcc clang git nasm wget curl zsh i3 -y;
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 chsh -s $(which zsh);
